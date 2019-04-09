f1e861533ad46179824724273fb18264828df0bf
Roberto Preste <roberto.preste@gmail.com>
2019-04-09 16:42:35 +0200

    First release with basic functionality

--- lunchtime/cli.py
+++ lunchtime/cli.py
@@ -3,14 +3,30 @@
 # Created by Roberto Preste
 import sys
 import click
+from lunchtime.functions import ignore_commands, clear_term
 
 
 @click.command()
-def main(args=None):
-    """Console script for lunchtime."""
-    click.echo("Replace this message by putting your code into "
-               "lunchtime.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option("--crazy", "-c", is_flag=True, default=False,
+              help="""Return crazy output on each command issued 
+              (default: False)""")
+@click.option("--silent", "-s", is_flag=True, default=False,
+              help="""Drop all messages when starting and exiting 
+              (default: False)""")
+@click.version_option()
+def main(crazy, silent):
+    """Ignore every command until 'exit' is issued.
+    """
+    if silent:
+        clear_term()
+    else:
+        clear_term("It's lunchtime!")
+    ignore_commands(crazy=crazy)
+    if silent:
+        clear_term()
+    else:
+        clear_term("Lunchtime's over :(")
+
     return 0
 
 
---
+++ lunchtime/functions.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import click
+import os
+import random
+import string
+import time
+from typing import Optional
+
+
+def _crazy_out(command: str) -> str:
+    """Return a weird string based on the given command.
+    :param str command: input command
+    :return: str
+    """
+    cr_command = "".join([chr(ord(el) + random.randrange(len(command)))
+                          + random.choice(string.ascii_letters)
+                          for el in command])
+    return cr_command
+
+
+def clear_term(msg: Optional[str] = None) -> Optional[str]:
+    """Clear the terminal and print an optional message.
+    :param Optional[str] msg: optional message to print after clearing
+    :return: Optional[str]
+    """
+    if msg:
+        click.echo()
+        click.echo(msg)
+        time.sleep(2)
+    os.system("clear")
+    return
+
+
+def ignore_commands(crazy: bool = False):
+    """Ignore all commands until 'exit' is issued.
+
+    Accept all kind of commands and gracefully ignore them, until the
+    'exit' command is issued.
+    :param bool crazy: return crazy output on each command (default: False)
+    :return:
+    """
+    command = " "
+    while command != "exit":
+        command = click.prompt(" ~ ")
+        if command != "exit" and crazy:
+            click.echo(_crazy_out(command))
+    return
